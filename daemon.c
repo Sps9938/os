84f8a0742e62758c5eb1fa4ed42add2629784901
SATYA PRAKASH SAHU <153068812+Sps9938@users.noreply.github.com>
2025-04-08 07:36:35 +0530

    Update daemon.c

--- daemon.c
+++ daemon.c
@@ -2,18 +2,51 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
     pid_t pid = fork();
-    if (pid < 0) exit(1);
-    if (pid > 0) exit(0);
+    if (pid < 0) {
+        perror("fork failed");
+        exit(1);
+    }
+    if (pid > 0) {
+        // Parent process exits
+        exit(0);
+    }
 
-    setsid();
-    chdir("/");
+    // Child continues
+    if (setsid() < 0) {
+        perror("setsid failed");
+        exit(1);
+    }
+
+    // Set file permissions mask
+    umask(0);
+
+    // Change working directory to root
+    if (chdir("/") < 0) {
+        perror("chdir failed");
+        exit(1);
+    }
+
+    // Close standard file descriptors
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
 
-    while (1) sleep(10);
+    // Open a log file to show it's working (optional)
+    int fd = open("/tmp/daemon.log", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd < 0) exit(1);
+
+    // Main daemon loop
+    while (1) {
+        dprintf(fd, "Daemon is alive: PID %d\n", getpid());
+        sleep(10);
+    }
+
+    close(fd);
     return 0;
 }
+

94d443a56fa8d87cd32c192964b1248d4f324d90
SATYA PRAKASH SAHU <153068812+Sps9938@users.noreply.github.com>
2025-04-08 11:55:08 +0530

    Update zombie.c

--- zombie.c
+++ zombie.c
@@ -3,7 +3,22 @@
 #include <stdlib.h>
 
 int main() {
-    if (fork() == 0) exit(0);
-    else { sleep(10); while(1); }
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child process
+        printf("Child process: exiting now.\n");
+        exit(0);
+    } else if (pid > 0) {
+        // Parent process
+        printf("Parent process: child has exited. Parent exists.\n");
+        sleep(10);
+        while (1);
+    } else {
+        // fork failed
+        perror("fork failed");
+        exit(1);
+    }
+
     return 0;
 }

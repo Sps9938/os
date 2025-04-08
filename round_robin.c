f91f4cf4ae99ac34edf6a09d81f24275aa5b45f2
SATYA PRAKASH SAHU <153068812+Sps9938@users.noreply.github.com>
2025-04-08 12:42:49 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
+
 int main() {
     int i, n, bt[10], rt[10], time, remain, tq, wt = 0, tat = 0;
-    printf("Enter number of processes: "); scanf("%d", &n); remain = n;
-    for (i = 0; i < n; i++) { printf("P[%d] BT: ", i+1); scanf("%d", &bt[i]); rt[i] = bt[i]; }
-    printf("Enter Time Quantum: "); scanf("%d", &tq);
-    for (time=0, i=0; remain!=0;) {
-        if (rt[i] > 0 && rt[i] <= tq) { time += rt[i]; rt[i] = 0; remain--; tat += time; wt += time - bt[i]; }
-        else if (rt[i] > 0) { rt[i] -= tq; time += tq; }
-        i = (i+1)%n;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    remain = n;  // Number of processes left to complete
+
+    for (i = 0; i < n; i++) {
+        printf("P[%d] BT: ", i + 1);
+        scanf("%d", &bt[i]);
+        rt[i] = bt[i];  // Copy burst time to remaining time
     }
-    printf("Avg WT = %.2f, TAT = %.2f\n", (float)wt/n, (float)tat/n);
+
+    printf("Enter Time Quantum: ");
+    scanf("%d", &tq);
+
+    // Round Robin Scheduling Loop
+    for (time = 0, i = 0; remain != 0;) {
+        if (rt[i] > 0 && rt[i] <= tq) {
+            time += rt[i];  // Process finishes
+            rt[i] = 0;
+            remain--;
+
+            tat += time;              // Turnaround time = current time
+            wt += time - bt[i];       // Waiting time = TAT - BT
+        }
+        else if (rt[i] > 0) {
+            rt[i] -= tq;  // Process partially runs
+            time += tq;
+        }
+
+        i = (i + 1) % n;  // Go to next process in a circular way
+    }
+
+    printf("Avg WT = %.2f, TAT = %.2f\n", (float)wt / n, (float)tat / n);
+
     return 0;
 }
+
